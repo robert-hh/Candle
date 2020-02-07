62f1c974361ddb578b68485c36200b120ad26c66
robert <robert@hammelrath.com>
2020-02-07 17:47:12 +0100

    candle.ino: Enforce consistent formatting using astyle

--- candle/candle.ino
+++ candle/candle.ino
@@ -38,23 +38,24 @@ void wdt_init(void)
     return;
 }
 
-void setup() {                
-  // initialize the digital pins
-#ifdef _AVR_IOTNX5_H_  
-    WDTCR |= (1<<WDIE); // reenable interrupt to prevent system reset
+void setup()
+{
+    // initialize the digital pins
+#ifdef _AVR_IOTNX5_H_
+    WDTCR |= (1 << WDIE); // reenable interrupt to prevent system reset
     // digitalWrite(LED_BOARD, LOW);
     power_usi_disable();
     ACSR |= _BV(ACD);                         //disable the analog comparator
 //    analogReference(ADC_Reference_Internal_1p1);
     analogReference(INTERNAL1V1);
-#else    
-    WDTCSR |= (1<<WDIE); // reenable interrupt to prevent system reset
+#else
+    WDTCSR |= (1 << WDIE); // reenable interrupt to prevent system reset
 #endif
-    pinMode(LED1, OUTPUT); //LED on Model A   
+    pinMode(LED1, OUTPUT); //LED on Model A
     pinMode(SENSE_PIN, INPUT);
-    pinMode(VCC_PIN, INPUT); 
+    pinMode(VCC_PIN, INPUT);
     pinMode(LDR, INPUT);
-#if DEBUGLED    
+#if DEBUGLED
     pinMode(STATUS, OUTPUT);
 #endif
 //    digitalWrite(LDR, HIGH);
@@ -70,7 +71,8 @@ void setup() {
 }
 
 // the loop routine runs over and over again forever:
-void loop() {
+void loop()
+{
     uint8_t index, mode;
     uint16_t offset, len, bright, vcc;
 
@@ -81,7 +83,7 @@ void loop() {
     digitalWrite(VCC_PIN, LOW);
     pinMode(VCC_PIN, INPUT);
 
-// Read LDR        
+// Read LDR
     pinMode(LDR, OUTPUT);
     digitalWrite(LDR, HIGH);
     bright = analogRead(SENSE);
@@ -95,14 +97,14 @@ void loop() {
 #if DEBUGLED
     digitalWrite(STATUS, vcc_state);
 #endif
-    
+
     if (ldr_state != 0 && vcc_state != 0) {  // LED flickers
         wdt_disable();
-        index = myrand(0) & 0x0f; 
+        index = myrand(0) & 0x0f;
         offset = pgm_read_word_near(offset_tbl + index) >> 1;
         len = (pgm_read_word_near(offset_tbl + index + 1) >> 1) - offset;
         section(offset, len);  // Display
-// Test LDR        
+// Test LDR
         if (bright > OFF_LIMIT) { // Daytime, switch off
             ldr_state = 0;
             analogWrite(LED1, 0); // Switch off
@@ -116,12 +118,12 @@ void loop() {
 
         system_sleep(WDTO_8S, SLEEP_MODE_PWR_DOWN);  // Sleep for 8 sec.
 
-// Test LDR      
+// Test LDR
         if ((ldr_state == 0) && (bright < ON_LIMIT)) {  // Nighttime, switch on
             ldr_state = 1;
         }
 // Test VCC, in case the battery recovers
-        if ((vcc_state == 0) && (vcc > VCC_HIGH)) {  
+        if ((vcc_state == 0) && (vcc > VCC_HIGH)) {
             vcc_state = 1;
         }
     }
@@ -140,40 +142,40 @@ void section(uint16_t offset, uint16_t len)
             val = 256 - val;
             val = 255 - ((val * val) >> 7);
         }
-        blink(val);  // 
+        blink(val);  //
     }
-    
+
 }
 // display a value
 void blink(uint16_t val)
 {
-        analogWrite(LED1, (val + 2 * prev) / 3); // interim step
-        delay(33);
-        analogWrite(LED1, (val * 2 + prev) / 3); // interim step
-        delay(33);
-        analogWrite(LED1, val);
-        delay(33);
-        prev = val;
+    analogWrite(LED1, (val + 2 * prev) / 3); // interim step
+    delay(33);
+    analogWrite(LED1, (val * 2 + prev) / 3); // interim step
+    delay(33);
+    analogWrite(LED1, val);
+    delay(33);
+    prev = val;
 }
 
 // Additive RNG along Donal Knuth
 
 uint8_t myrand(uint8_t noise)
 {
-    static uint8_t set[] = { 
-            21,  49, 217,  45,   6,  22, 161,  31,  61, 231, 
-           169,  17,  42, 173, 218, 194, 186, 170,  20,  89, 
-            44, 138, 163,  58,  30,  34, 220,  31, 134, 130, 
-            36,  42,  20,  98, 246,  23, 124, 121,  48,  16, 
-            92,   8,  82,  64,  94,  11, 250,  64, 214, 164, 
-            88,  87,  67, 225,  23
+    static uint8_t set[] = {
+        21,  49, 217,  45,   6,  22, 161,  31,  61, 231,
+        169,  17,  42, 173, 218, 194, 186, 170,  20,  89,
+        44, 138, 163,  58,  30,  34, 220,  31, 134, 130,
+        36,  42,  20,  98, 246,  23, 124, 121,  48,  16,
+        92,   8,  82,  64,  94,  11, 250,  64, 214, 164,
+        88,  87,  67, 225,  23
     };
     static uint16_t k = 54;
     static uint16_t j = 23;
     static uint16_t modulus = 256;
     uint8_t retval;
     int8_t i;
-    
+
     if (noise != 0) {
         for (i = 0; i < sizeof(set); i++) {
             set[i] = (set[i] + noise) % modulus;
@@ -187,33 +189,33 @@ uint8_t myrand(uint8_t noise)
     return retval;
 }
 
-//****************************************************************  
-// set system into the sleep state 
+//****************************************************************
+// set system into the sleep state
 // system wakes up when wtchdog is timed out
-void system_sleep(int duration, int mode) 
+void system_sleep(int duration, int mode)
 {
     wdt_enable(duration);
     set_sleep_mode(mode); // sleep mode is set here
     wdt_reset();
-#ifdef _AVR_IOTNX5_H_  
-    WDTCR |= (1<<WDIE); // reenable interrupt to prevent system reset
+#ifdef _AVR_IOTNX5_H_
+    WDTCR |= (1 << WDIE); // reenable interrupt to prevent system reset
     ADCSRA &= ~_BV(ADEN);                     //disable ADC
-#else    
-    WDTCSR |= (1<<WDIE); // reenable interrupt to prevent system reset
+#else
+    WDTCSR |= (1 << WDIE); // reenable interrupt to prevent system reset
 #endif
 
     sleep_enable();
     sleep_bod_disable();
     sleep_cpu();
-    sleep_disable();                     // System continues execution here when watchdog timed out 
-#ifdef _AVR_IOTNX5_H_  
+    sleep_disable();                     // System continues execution here when watchdog timed out
+#ifdef _AVR_IOTNX5_H_
     ADCSRA |= _BV(ADEN);                     //enable ADC
-#endif    
+#endif
 }
 
-//****************************************************************  
+//****************************************************************
 
-ISR(WDT_vect) 
+ISR(WDT_vect)
 {
-   wdt_disable();
+    wdt_disable();
 }
